6b0c56abfdb6787f6c69f4073be12ee3c463c56c
Appu Sidharth <appu@nference.net>
2021-07-03 17:19:50 +0530

    working dummy package model

--- site.py
+++ site.py
@@ -2,7 +2,7 @@ from Config import Config
 import json
 import getpass
 import paramiko
-
+import sys
 class site(Config):
 
     def __init__(self):
@@ -12,7 +12,7 @@ class site(Config):
         self.manifest_file=self.get_property('MANIFEST_FILE')
 
 
-    def get_connect_data(self,hosts,hostname,property):
+    def get_connect_data(self,host_groups):
         print(self.host_file)
         try:
             f=open(self.host_file,'r')
@@ -23,10 +23,11 @@ class site(Config):
             data=json.load(f)
         except:
             print("JSON load failef for file" + self.host_file)
-        return data[hosts][hostname][property]
+        return data[host_groups]
 
     def get_exec_data(self,play_name):
         print(self.manifest_file)
+        self.play_name=play_name
         try:
             f=open(self.manifest_file,'r')
         except IOError:
@@ -36,8 +37,7 @@ class site(Config):
             data=json.load(f)
         except:
             print("JSON load failef for file" + self.host_file)
-        for key in data['play'][play_name]:
-            print(key)
+        return data['play'][play_name]
 
  
 
@@ -47,33 +47,62 @@ class connect(site):
         super().__init__()
         print("connecting "+self.host_file)
 
-    def ssh(self,hosts,hostname,command):
-        self.ssh_hostname=self.get_connect_data(hosts,hostname,'ip')
-        self.ssh_user=self.get_connect_data(hosts,hostname,'user')
-        try:
-            self.ssh_password = getpass.getpass()
-        except Exception as error:
-            print('ERROR', error)
+    def ssh(self,connect_data,command):
+        for key, host in connect_data.items():
+            print(host)
+            self.ssh_hostname=host['ip']
+            self.ssh_user=host['user']
+            try:
+                self.ssh_password = getpass.getpass()
+            except Exception as error:
+                print('ERROR', error)
 
-        ssh = paramiko.SSHClient() 
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        try:
-            ssh.connect(self.ssh_hostname, username=self.ssh_user, password=self.ssh_password)
-        except:
-            print("[!] Cannot connect to the SSH Server")
-        
-        ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(command)
-        #except:
-        if ssh_stdout.channel.recv_exit_status() != 0:
-            print("Command execution failed \n--- \ncommand:%s \nstdout:%s \nstderr:%s \n---" %(command, ssh_stdout.read().decode(), ssh_stderr.read().decode()))
-        
-        print(ssh_stdout.read().decode())
+            ssh = paramiko.SSHClient() 
+            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            try:
+                ssh.connect(self.ssh_hostname, username=self.ssh_user, password=self.ssh_password)
+            except:
+                print("[!] Cannot connect to the SSH Server")
+            
+            ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(command)
+            #except:
+            if ssh_stdout.channel.recv_exit_status() != 0:
+                print("Command execution failed \n--- \ncommand:%s \nstdout:%s \nstderr:%s \n---" %(command, ssh_stdout.read().decode(), ssh_stderr.read().decode()))
+            
+            print(ssh_stdout.read().decode())
 
 
-#class packages(site)
+class packages(connect):
+    def __init__(self):
+        super().__init__()
+        print("packages ")
+    def package_manager(self,package_list):
+        dir(package_list)
+        for package_name, status in package_list.items():
+            if status == "present":
+                print("installing package %s with available latest version" %package_name)
+                connect_data=self.get_connect_data(self.play_name)
+                print(connect_data)
+                print('------------------')
+                self.ssh(connect_data,'date')
+            elif status == "absent":
+                print("Removing package %s" %package_name)
+            else:
+                print("installing package %s with version %s" %(package_name,status))
+
+
 if __name__ == '__main__':
-    obj=connect()
-    obj.ssh('webservers','host1','date1')
-    obj.get_exec_data('basic-install')
+    obj=packages()
+#    obj.ssh('webservers','host1','date1')
+    mods=obj.get_exec_data('webservers')
+    for mod,key in mods.items():
+        if mod=='packages':
+            print(key)
+            #actions=packages()
+            obj.package_manager(key)
+        else:
+            print("unsupported module")
+
+            
    # print(str(obj.get_host_ip('webservers','host1','ip')))
 
