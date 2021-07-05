7c22ae91b06d975da932264c07f9280db342e529
Appu <appu@nference.net>
2021-07-05 20:59:07 +0530

    print clean up

--- main.py
+++ main.py
@@ -5,16 +5,17 @@ if __name__ == '__main__':
     mods=obj.get_exec_data('webservers')
     connect_data=obj.get_connect_data('webservers')
     for connect in connect_data.items():
-        print("running actions on =================%s:%s================" %(connect[0], connect[1]['ip']))
+        print("\n\n==================================%s:%s=================================\n" %(connect[0], connect[1]['ip']))
         obj.set_connect_data(connect) # I messed up, ssh function needed to return std err and out but I was sending multiple host data at once. spliting the data and looping it and setting it back with single host again here.
         for mod,key in mods.items():
             if mod=='packages':
-                #actions=packages()
-                #obj.package_manager(key)
-                print("skip")
+                print("-------------PACKAGES-START-------------")
+                obj.package_manager(key)
+                print("-------------PACKAGES-FINISH------------\n\n")
             elif mod=='files':
-                #print(key)
+                print("-------------FILES-START----------------")
                 obj.file_manager(key)
+                print("-------------FILES-FINISH---------------\n\n")
             else:
                 print("unsupported module")
 
